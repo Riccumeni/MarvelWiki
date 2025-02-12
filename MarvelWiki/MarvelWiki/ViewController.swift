40cda251c1117c2ca6c126120d79fa9ab02024cc
Alessandro Riccumeni <alessandro.riccumeni@orbyta.it>
2025-02-12 16:26:05 +0100

    Initialize project

---
+++ MarvelWiki/MarvelWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelWiki
+//
+//  Created by Alessandro Riccumeni on 12/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

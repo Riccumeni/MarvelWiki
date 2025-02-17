26ed74661e1af395e4d0d96676e7bf5a4c266e83
Alessandro Riccumeni <alessandro.riccumeni@orbyta.it>
2025-02-17 17:51:12 +0100

    chore: Adjustment to newInstance

--- MarvelWiki/MarvelWiki/Scenes/_Common/Extensions/Extension+UIViewController.swift
+++ MarvelWiki/MarvelWiki/Scenes/_Common/Extensions/Extension+UIViewController.swift
@@ -18,11 +18,13 @@ extension UIViewController {
     /// - Returns: A new instance of the view controller with the specified title and tab bar image.
     ///
     /// - Note: This method assumes that the view controller can be initialized using `init()`. If the view controller is instantiated from a storyboard, this method may not work correctly.
-    static func newInstance(title: String, imageName: String) -> UIViewController {
+    static func newInstance(title: String, imageName: String? = nil) -> UIViewController {
         let vc = self.init()
         
         vc.title = title
-        vc.tabBarItem.image = UIImage(systemName: imageName)
+        if let imageName {
+            vc.tabBarItem.image = UIImage(systemName: imageName)
+        }
         
         return vc
     }
